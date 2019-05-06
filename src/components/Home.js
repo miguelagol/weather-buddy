090d37d0af346703fe9edc885f20877b43f132ec
miguelagol <michalina.gol@gmail.com>
2019-05-06 17:02:38 +0200

    Add a HOC Redirect to switch path to /forecast

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Button from './Button.js';
 import Input from './Input.js';
 import css from './Home.module.scss';
 
-export default function Home() {
-   return (
-      <div className={css.home}>
-         <h2 className={css.title}>Enter a City</h2>
-         <Input className={css['responsive-width']} variant="big" />
-         <Button type="button" variant="big">
-            Get Weather
-         </Button>
-      </div>
-   );
+export default class Home extends React.Component {
+   state = {
+      toForecast: false,
+   };
+
+   handleClick = () => {
+      this.setState({ toForecast: true });
+   };
+
+   render() {
+      const { toForecast } = this.state;
+
+      if (toForecast === true) {
+         return <Redirect to="/forecast" />;
+      }
+      return (
+         <div className={css.home}>
+            <h2 className={css.title}>Enter a City</h2>
+            <Input className={css['responsive-width']} variant="big" />
+            <Button type="button" variant="big" onClick={this.handleClick}>
+               Get Weather
+            </Button>
+         </div>
+      );
+   }
 }
