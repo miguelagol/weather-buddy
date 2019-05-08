946a368783f44f3e1492e8dcffdef5c4790030fe
miguelagol <michalina.gol@gmail.com>
2019-05-08 22:39:18 +0200

    Simplify forcast data mapper

--- src/utils/forecastDataMapper.js
+++ src/utils/forecastDataMapper.js
@@ -1,59 +1,36 @@
+import { format } from 'date-fns';
+
+function getTemperature(tempMin, tempMax) {
+   const tempAverage = (tempMin + tempMax) / 2;
+   return Math.round(tempAverage);
+}
+
+function getWeekDay(date) {
+   return format(date, 'dddd');
+}
+
+function getDate(date) {
+   return format(date, 'DD.MM');
+}
+
+function getTime(date) {
+   return format(date, 'HH:mm');
+}
+
 export default function forecastDataMapper(fetchData) {
-   const list = fetchData.list;
-   const listArray = Array.from(list);
+   const listArray = fetchData.list;
 
-   const weatherData = [];
+   return listArray.map(({ dt_txt, main, weather }) => {
+      const date = new Date(dt_txt);
+      const { description, icon } = weather[0];
 
-   for (let i = 0; i < listArray.length; i++) {
-      weatherData.push({
-         day: getWeekDay(listArray[i].dt_txt),
-         date: getDate(listArray[i].dt_txt),
-         time: getTime(listArray[i].dt_txt),
-         temperature: roundTemperature(listArray[i].main.temp),
-         temperatureMax: roundTemperature(listArray[i].main.temp_max),
-         temperatureMin: roundTemperature(listArray[i].main.temp_min),
-         weatherDescription: listArray[i].weather[0].description,
-         weatherIcon: listArray[i].weather[0].icon,
-      });
-   }
-
-   function roundTemperature(temp) {
-      return Math.round(temp);
-   }
-
-   function getWeekDay(dateString) {
-      const days = [
-         'Sunday',
-         'Monday',
-         'Tuesday',
-         'Wednesday',
-         'Thursday',
-         'Friday',
-         'Saturday',
-      ];
-
-      const date = new Date(dateString);
-
-      return days[date.getDay()];
-   }
-
-   function getDate(dateString) {
-      let date = new Date(dateString);
-      date = ['0' + date.getDate(), '0' + (date.getMonth() + 1)].map(newDate =>
-         newDate.slice(-2)
-      );
-
-      return date.slice(0, 2).join('.');
-   }
-
-   function getTime(dateString) {
-      let time = new Date(dateString);
-      time = ['0' + time.getHours(), '0' + time.getMinutes()].map(newTime =>
-         newTime.slice(-2)
-      );
-
-      return time.slice(0, 2).join(':');
-   }
-
-   return weatherData;
+      return {
+         day: getWeekDay(date),
+         date: getDate(date),
+         time: getTime(date),
+         temperature: getTemperature(main.temp_min, main.temp_max),
+         weatherDescription: description,
+         weatherIcon: icon,
+      };
+   });
 }
