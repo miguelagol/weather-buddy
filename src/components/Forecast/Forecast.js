a81fead5268de6f1d227e4eb920f1e5fcf9136ea
miguelagol <michalina.gol@gmail.com>
2019-05-10 13:27:52 +0200

    Change class Day to function

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -59,41 +59,37 @@ export default class Forecast extends React.Component {
    }
 }
 
-class Day extends React.Component {
-   static propTypes = {
-      day: PropTypes.string.isRequired,
-      date: PropTypes.string.isRequired,
-      time: PropTypes.string.isRequired,
-      temperature: PropTypes.number.isRequired,
-      weatherDescription: PropTypes.string.isRequired,
-      weatherIcon: PropTypes.string.isRequired,
-   };
-
-   render() {
-      const {
-         day,
-         date,
-         time,
-         temperature,
-         weatherDescription,
-         weatherIcon,
-      } = this.props;
-
-      return (
-         <div className={css.day}>
-            <h3 className={css.date}>
-               {day} {date}
-            </h3>
-            <h2 className={css.time}>{time}</h2>
-            <div>Temp. : {temperature}&deg;C</div>
-            <div>
-               {weatherDescription}
-               <img
-                  src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
-                  alt="Weather icon"
-               />
-            </div>
+function Day({
+   day,
+   date,
+   time,
+   temperature,
+   weatherDescription,
+   weatherIcon,
+}) {
+   return (
+      <div className={css.day}>
+         <h3 className={css.date}>
+            {day} {date}
+         </h3>
+         <h2 className={css.time}>{time}</h2>
+         <div>Temp. : {temperature}&deg;C</div>
+         <div>
+            {weatherDescription}
+            <img
+               src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
+               alt="Weather icon"
+            />
          </div>
-      );
-   }
+      </div>
+   );
 }
+
+Day.propTypes = {
+   day: PropTypes.string.isRequired,
+   date: PropTypes.string.isRequired,
+   time: PropTypes.string.isRequired,
+   temperature: PropTypes.number.isRequired,
+   weatherDescription: PropTypes.string.isRequired,
+   weatherIcon: PropTypes.string.isRequired,
+};
